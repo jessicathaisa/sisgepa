df3c42a8c36a2a0ef9468ad50329259f49fa5f68
Jessica Thaisa Silva de Almeida <jessicatsalmeida@gmail.com>
2016-05-07 11:08:35 -0300

    Incluindo angularjs e bootstrap. Feito relatorio.

---
+++ web/js/app.js
@@ -0,0 +1,20 @@
+/* 
+ * Projeto SISGEPA - Sistema de Gestao de Producao Academica
+ * Disciplina Projeto de Sistemas de Software
+ * Professor Carlos J. P. de Lucena
+ * PUC-Rio 2016.1
+ */
+var app = angular.module('sisgepa', []);
+
+app.controller('RelatorioController', RelatorioController);
+
+function RelatorioController($scope, $http) {
+    this.relatorio = {};
+    $http.post('RelatorioServlet').
+            success(function (data) {
+                $scope.relatorio = data;
+            }).
+            error(function (data) {
+                // log error
+            });
+}
---
+++ web/js/geral.js
@@ -0,0 +1,12 @@
+/* 
+ * Projeto SISGEPA - Sistema de Gestao de Producao Academica
+ * Disciplina Projeto de Sistemas de Software
+ * Professor Carlos J. P. de Lucena
+ * PUC-Rio 2016.1
+ */
+
+
+$(function(){
+  $("#header").load("inc/header.html"); 
+  $("#footer").load("inc/footer.html"); 
+});
